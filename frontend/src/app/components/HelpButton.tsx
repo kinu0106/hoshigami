dae28a5b0968c33add482c0e991b26e24f304457
Kinu <hk.business20250901@gmail.com>
2026-04-01 14:36:09 +0900

    操作説明ボタンの修正反映

--- frontend/src/app/components/HelpButton.tsx
+++ frontend/src/app/components/HelpButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export default function HelpButton() {
   const pathname = usePathname();
@@ -25,7 +26,7 @@ export default function HelpButton() {
   const helpUrl = `/help#${helpSection}`;
 
   return (
-    <a
+    <Link
       href={helpUrl}
       className="btn btn-sm d-flex align-items-center gap-2 help-button help-button-custom text-decoration-none"
       title="操作説明を見る"
@@ -37,6 +38,6 @@ export default function HelpButton() {
         <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.326 0-2.786.647-2.754 2.533zm1.326 7.743c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94z" />
       </svg>
       <span className="help-button-text">操作説明</span>
-    </a>
+    </Link>
   );
 }
